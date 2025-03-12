a98a26fec363990306dc0644527800b5150c4c7b
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-03-12 10:32:28 +0800

    Handle quantiy & Multiple products

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -19,7 +19,7 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     }
 
     // Handle success add to cart
-    toast.success(`${item.name} added to cart`, {
+    toast.success(res.message, {
       action: {
         label: "Go To Cart",
         onClick: () => router.push("/cart"),
