bcf1d11efca227f0b2076212808da9b6db3f7023
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-03-14 13:30:20 +0800

    Shipping Address Zod Schema & Page

---
+++ app/(root)/shipping-address/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from "@/auth";
+import { getMyCart } from "@/lib/actions/cart-action";
+import { getUserById } from "@/lib/actions/users.actions";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { ShippingAddress } from "@/types";
+
+export const metadata: Metadata = {
+  title: "Shipping Address",
+};
+
+const ShippingAddressPage = async () => {
+  const cart = await getMyCart();
+  if (!cart || cart.items.length === 0) redirect("/cart");
+
+  const session = await auth();
+
+  const userId = session?.user?.id;
+  if (!userId) throw new Error("No User ID");
+
+  const user = await getUserById(userId);
+
+  return <>Address</>;
+};
+
+export default ShippingAddressPage;
--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,7 @@ import {
   insertProductSchema,
   insertCartSchema,
   cartItemSchema,
+  shippingAddressSchema,
 } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -10,6 +11,7 @@ export type Product = z.infer<typeof insertProductSchema> & {
   rating: string;
   createdAt: Date;
 };
-
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
+
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
